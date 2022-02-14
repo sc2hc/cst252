9c308e9cc24b5d053667a25e0785329e5d549888
sc2hc <hchan@csumb.edu>
2022-02-14 01:12:22 -0800

    lab 5
    css, js, and html

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+
+/**
+ * Author:   Henry
+ * Created:   2/12/2022
+ * public domain
+ *
+ *
+ **/
+
+ make = "Ford";
+ model = "Taurus";
+ color = "Silver";
+ year = 1995;
+
+ age = 2022 - year;
+
+document.writeln("Make: "+ make + "<br>");
+ document.writeln("MOdel: "+ model + "<br>");
+ document.writeln("Year: "+ year + "<br>");
+ document.writeln("Color: "+ color + "<br>");
+ document.writeln("Age: "+ age + "<br>");
