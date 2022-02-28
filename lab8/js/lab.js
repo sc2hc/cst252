8ff37d9e92911a31786a2ea1b8b406c472bf3ee4
sc2hc <hchan@csumb.edu>
2022-02-28 10:05:59 -0800

    lab8
    html css and js for lab regarding a odd checking function

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,7 +1,7 @@
 
 /**
  * Author:   Henry
- * Created:   9/29/2021
+ * Created:   2/26/2022
  * public domain
  *
  *
@@ -26,17 +26,17 @@
  outputEl.appendChild(newEl);
 
  var result = array.map(isOdd);
- // should return [true, false, true, true, true, true, true]
- console.log("Test of evenness of array:", result);
+ // should return [false, true, true, true, false, true, true]
+ console.log("Test of oddness of array:", result);
  // add array to document
  var newEl = document.createElement("p");
- newEl.innerHTML = "Evenness of array:" + JSON.stringify(result);
+ newEl.innerHTML = "Oddness of array:" + JSON.stringify(result);
  outputEl.appendChild(newEl);
 
  var result = array.map(function(x){
     return x ** 0.5;
  })
- // should return [10, 9, 2, 4, 6.48074069840786, 12, 100]
+
  console.log("Squareroot of array:", result);
  // add array to document
  var newEl = document.createElement("p");
