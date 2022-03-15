3a5ebabef6d6d6a2c5e124c03973f8829aa97751
sc2hc <hchan@csumb.edu>
2022-03-14 23:09:11 -0700

    Lab 12 and 13
    Full js on 12 and starting lab 13

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -1,11 +1,43 @@
 
 /**
  * Author:   Henry
- * Created:   10/17/2021
+ * Created:   3/11/2021
  * public domain
  *
  *
  **/
 
-$("#output").html("<button id=my-button>Press me")
-$("#my-button").click(function(){$("#output").append("<button id=my-button>Press me")})
+array=["yo","meep","pain","shindemoii"];
+var count1 = 0;
+var count2 = 0;
+var count3 = 0;
+
+$("#hashirei").click(function(){
+  $("#challenge").addClass(array[count1]);
+  count1++;
+  if (count1 > array.length) {
+		count1 = 0;
+    $("#challenge").removeClass("yo meep pain shindemoii");
+	}
+  }
+)
+
+$("#nigeru").click(function(){
+  $("#problemo").addClass(array[count2]);
+  count2++;
+  if (count2 > array.length) {
+		count2 = 0;
+    $("#problemo").removeClass("yo meep pain shindemoii");
+	}
+  }
+)
+
+$("#totsugeki").click(function(){
+  $("#results").addClass(array[count3]);
+  count3++;
+  if (count3 > array.length) {
+		count3 = 0;
+    $("#results").removeClass("yo meep pain shindemoii");
+	}
+  }
+)
